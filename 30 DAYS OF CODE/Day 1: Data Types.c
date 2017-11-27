60f0a0ccf402531845291e5938aad85e6794e2f5
MarouaneSK <33138118+MarouaneSK@users.noreply.github.com>
2017-11-27 14:32:20 +0100

    Create Day 1: Data Types.c

---
+++ 30 DAYS OF CODE/Day 1: Data Types.c
@@ -0,0 +1,22 @@
+
+    // Declare second integer, double, and String variables.
+    int a,k;double l, b; char c[300] ;
+    // Read and save an integer, double, and String to your variables.
+   
+    scanf("%d",&a);
+    
+    scanf("%lf",&b);
+    scanf("%c",&c);
+
+    fgets(c,300,stdin);
+    // Print the sum of both integer variables on a new line.
+    k=a+i;
+    printf("%d\n",k);
+    // Print the sum of the double variables on a new line.
+    l=d+b;
+    printf("%.1lf\n",l);
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    printf("%s",s);
+    
+    printf("%s",c);
