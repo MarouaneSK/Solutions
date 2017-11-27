e33516dec11163bad8f9d8f0b14667d616241d61
MarouaneSK <33138118+MarouaneSK@users.noreply.github.com>
2017-11-27 14:30:03 +0100

    Create Hello World.c

---
+++ 30 DAYS OF CODE/Hello World.c
@@ -0,0 +1,8 @@
+int main() {
+    // Declare a variable named 'input_string' to hold our input.
+    char input[105], c; 
+    fgets (input , 105 , stdin);
+    
+    printf("Hello, World.\n");
+    printf("%s",input);
+}
